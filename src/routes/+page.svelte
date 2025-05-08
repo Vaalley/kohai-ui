4f14d92a312ec12ca4f4e3473ed34443e5da3f26
vaalley <vaaleyou@gmail.com>
2025-05-08 23:40:16 +0200

    feat: create test page with UI component showcase and styling examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,24 @@
 </svelte:head>
 
 <main>
-	<h1>Test header 1</h1>
-	<h2>Test header 2</h2>
-	<h3>Test header 3</h3>
-	<h4>Test header 4</h4>
-	<p>
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, laudantium atque. Perspiciatis, maxime temporibus nemo assumenda doloremque quasi
-		tempore laboriosam?
-	</p>
+	<section title="Logo">
+		<img src="Logo.svg" alt="Logo">
+	</section>
+
+	<section title="Headers">
+		<h3>These are headers</h3>
+		<h1>Test header 1</h1>
+		<h2>Test header 2</h2>
+		<h3>Test header 3</h3>
+		<h4>Test header 4</h4>
+	</section>
+
+	<section title="Paragraph">
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, laudantium atque. Perspiciatis, maxime temporibus nemo assumenda doloremque quasi
+			tempore laboriosam?
+		</p>
+	</section>
 
 	<section title="Button sizes">
 		<Button size="lg"><Mail size="1em" /> Test button lg</Button>
