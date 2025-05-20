fc204603dab8ef1ff94626e428abfceeab1c6100
Valentin <61027346+Vaalley@users.noreply.github.com>
2025-05-20 14:49:43 +0200

    refactor: use CSS variable for separator width instead of inline style

--- src/lib/components/atoms/Separator.svelte
+++ src/lib/components/atoms/Separator.svelte
@@ -2,11 +2,12 @@
 	let { width = "100px" }: { width?: string } = $props();
 </script>
 
-<div class="separator" style="width: {width}"></div>
+<div class="separator" style="--width: {width}"></div>
 
 <style lang="scss">
 	.separator {
 		height: var(--border-width);
+		width: var(--width);
 		background-color: var(--gray);
 	}
 </style>
